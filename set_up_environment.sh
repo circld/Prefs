a3df79072fa65d5c77ddc38f04bd9f5b3aebca12
circld <circld1@gmail.com>
2016-03-22 19:38:54 -0600

    incorporate new dotfiles into set_up_environment.sh

--- set_up_environment.sh
+++ set_up_environment.sh
@@ -5,6 +5,9 @@
 # add vim plugins
 ./add_all_submodules.sh
 
+# install fonts
+sh ~/.vim/bundle/vim-airline-fonts/install.sh
+
 # replace snippets with customs snippets
 cp -f ~/Prefs/snippets/* ~/.vim/bundle/vim-snippets/snippets
 
@@ -39,6 +42,31 @@ then
 else
     echo "creating ~/.bash_profile ..."
     echo "source ~/Prefs/.bash_profile" > ~/.bash_profile
-    echo "source ~/.dotfiles/command_prompt.sh" >> ~/.bash_profile
-    cp ~/.bash_profile ~/.bashrc
+fi
+
+# add placeholder .bashrc if doesn't exist
+if [ -e ~/.bashrc ] 
+then
+    echo ".bashrc exists; skipping .bashrc placeholder file creation step."
+else
+    echo "creating ~/.bashrc ..."
+    echo "source ~/Prefs/.bashrc" > ~/.bashrc
+fi
+
+# add placeholder .inputrc if doesn't exist
+if [ -e ~/.inputrc ] 
+then
+    echo ".inputrc exists; skipping .inputrc placeholder file creation step."
+else
+    echo "creating ~/.inputrc ..."
+    echo "\$include ~/Prefs/.inputrc" > ~/.inputrc
+fi
+
+# add placeholder .tmux.conf if doesn't exist
+if [ -e ~/.tmux.conf ] 
+then
+    echo ".tmux.conf exists; skipping .tmux.conf placeholder file creation step."
+else
+    echo "creating ~/.tmux.conf ..."
+    echo "source-file ~/Prefs/.tmux.conf" > ~/.tmux.conf
 fi
