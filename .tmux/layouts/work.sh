7c9a493726416bf60fc50e035be7ac7cefc89586
circld <circld1@gmail.com>
2016-04-13 09:55:32 -0600

    remove ref to python2 from work.sh & laptop.sh

--- .tmux/layouts/work.sh
+++ .tmux/layouts/work.sh
@@ -12,6 +12,6 @@ tmux split-window -h
 tmux select-pane -t 0
 tmux split-window -h -p 25
 tmux send-keys -t $name:0.0 vi C-m
-tmux send-keys -t $name:0.1 ipython2 C-m
+tmux send-keys -t $name:0.1 ipython C-m
 tmux send-keys -t $name:0.4 'hive -S' C-m
 tmux attach-session -d -t $name
