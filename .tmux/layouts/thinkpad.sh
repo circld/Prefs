e8ba0aa134176e3973545b6a113fc2efc5a9a2dc
circld <circld1@gmail.com>
2016-04-12 22:28:50 -0600

    tweak layout for arch box

--- .tmux/layouts/thinkpad.sh
+++ .tmux/layouts/thinkpad.sh
@@ -5,7 +5,7 @@ else
     name=$1
 fi
 tmux new-session -s $name -d
-tmux split-window -h -p 30
+tmux split-window -h -p 45
 tmux split-window
 tmux send-keys -t $name:0.2 ipython2 C-m
 tmux send-keys -t $name:0.0 vi C-m
