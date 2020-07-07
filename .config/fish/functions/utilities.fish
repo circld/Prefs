3ae7f6479a4d6a5baa88930244bf876c75af7d3a
circld <circld1@gmail.com>
2020-07-07 17:54:32 -0500

    add `brb` CLI fish command

--- .config/fish/functions/utilities.fish
+++ .config/fish/functions/utilities.fish
@@ -5,3 +5,8 @@ function . --description '`cd` up $argv[1] times and then to path in $argv[2]'
   set d_list $d_list $argv[2]
   cd (string join / $d_list)
 end
+
+function brb --description 'cd into a directory, execute a command, then cd back. n.b., must escape wildcards with `\` or use quotes.'
+  cd $argv[1] && eval $argv[2..-1]
+  cd -
+end
