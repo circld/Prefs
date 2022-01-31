abe8aa1d620b061319cf483b0a0df767afd79dc3
circld <circld1@gmail.com>
2022-01-31 14:27:08 -0800

    vif: match against full path

--- .config/fish/functions/utilities.fish
+++ .config/fish/functions/utilities.fish
@@ -42,7 +42,7 @@ function preview --description 'preview files using fzf'
 end
 
 function vif --description 'fuzzy find a file in directory or subdirectories and open in neovim'
-  set found (fd $argv[1..-1] --type=f --color=never | preview)
+  set found (fd $argv[1..-1] --type=f --full-path --color=never | preview)
   if test $status -eq 0
     nvim $found
   end
