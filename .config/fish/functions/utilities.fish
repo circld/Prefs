87a4a5e30591d01466b32ff07d0aacfaf2c53d7f
circld <circld1@gmail.com>
2022-01-31 14:40:08 -0800

    cdf: better paging speed

--- .config/fish/functions/utilities.fish
+++ .config/fish/functions/utilities.fish
@@ -49,8 +49,10 @@ function vif --description 'fuzzy find a file in directory or subdirectories and
 end
 
 function cdf --description 'fuzzy find a directory or subdirectory and move there'
-  set found (fd $argv[1..-1] --absolute-path --type=d --color=never |
-    fzf --layout reverse --preview "fd --color=always --base-directory {1} ."
+  set found (fd $argv[1..-1] --full-path --type=d --color=never |
+    fzf --layout reverse \
+       --preview "fd --color=always --base-directory {1} ." \
+       --bind=shift-down:preview-half-page-down,shift-up:preview-half-page-up
   )
   if test $status -eq 0
     cd $found
