d5708ae9d1e20514504905377d3baa2bcd3434ea
circld <circld1@gmail.com>
2022-03-30 09:41:55 -0700

    cdf: improve default sort order of results

--- .config/fish/functions/utilities.fish
+++ .config/fish/functions/utilities.fish
@@ -49,7 +49,11 @@ function vif --description 'fuzzy find a file in directory or subdirectories and
 end
 
 function cdf --description 'fuzzy find a directory or subdirectory and move there'
+  # sort matching directories from least to most nested
   set found (fd $argv[1..-1] --full-path --type=d --color=never |
+    awk '{ split($0, parts, "/"); print length(parts), $0 }' |
+    sort -n |
+    awk '{ print $2 }' |
     fzf --layout reverse \
        --preview "fd --color=always --base-directory {1} ." \
        --bind=shift-down:preview-half-page-down,shift-up:preview-half-page-up
